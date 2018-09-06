f458c8f78b961929097a0f5c48d265902bad018c
Darko Tasevski <c.dtasevski@gogoair.com>
2018-09-06 21:48:22 +0200

    fix: Header position

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,18 +8,16 @@ import Button from '../Common/Button';
 export default class Header extends Component {
 	render() {
 		return (
-			<header>
-				<nav className={styles.Nav}>
-					<Logo />
-					<div className={styles.Nav__links}>
-						<NavLink to="/">News</NavLink>
-						<NavLink to="/">Nature</NavLink>
-						<NavLink to="/">Travel</NavLink>
-						<NavLink to="/">Culture</NavLink>
-					</div>
-					<Button />
-				</nav>
-			</header>
+			<nav className={styles.Nav}>
+				<Logo />
+				<div className={styles.Nav__links}>
+					<NavLink to="/">News</NavLink>
+					<NavLink to="/">Nature</NavLink>
+					<NavLink to="/">Travel</NavLink>
+					<NavLink to="/">Culture</NavLink>
+				</div>
+				<Button />
+			</nav>
 		);
 	}
 }
